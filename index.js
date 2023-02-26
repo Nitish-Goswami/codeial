26ab549d6d00a9a50b58ddec66d561fcd46fd45e
nitish-devop <devnitish97@gmail.com>
2023-02-26 16:37:14 +0530

    First Commit : Create Directory Structure and Setup Express Server

---
+++ index.js
@@ -0,0 +1,8 @@
+const express = require('express');
+const app = express();
+const port = 5000;
+
+app.listen(port, function(err){
+    if(err) console.log(`Error : ${err}`);
+    console.log('Server is running on port ',port);
+})
