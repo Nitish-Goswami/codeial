e1f1447fec489f82524df331feeffbdc381ca9d6
nitish-devop <devnitish97@gmail.com>
2023-02-26 16:59:05 +0530

    Set up view engine

--- index.js
+++ index.js
@@ -2,7 +2,8 @@ const express = require('express');
 const app = express();
 const port = 4000;
 
-app.set('view-engine','ejs');
+// Set Up view engine
+app.set('view engine','ejs');
 app.set('views','./views');
 
 app.use('/',require('./routes/index'));
